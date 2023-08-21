22a947b99da35b2e05b1e623f11d713d86979335
Jay Cabasag <jaycabasag1999@gmail.com>
2023-08-21 09:44:21 +0800

    feat: set env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,23 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entity/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '194.163.35.151',
+      host: process.env.DATABASE_HOST,
       port: 3306,
-      username: 'u521316269_univtraze',
-      password: 'Univtraze123!!',
-      database: 'u521316269_univtraze',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_USERNAME,
       entities: [User],
       synchronize: true,
     }),
