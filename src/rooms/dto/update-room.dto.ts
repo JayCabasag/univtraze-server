d677479063c2342eb23075bb97b0d0fbb6f7ecbc
Jay Cabasag <jaycabasag1999@gmail.com>
2023-09-02 21:03:06 +0800

    feat: added update

--- src/rooms/dto/update-room.dto.ts
+++ src/rooms/dto/update-room.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoomDto } from './create-room.dto';
+import * as Joi from 'joi';
+
+export const UpdateRoomSchema = Joi.object({
+  building_number: Joi.string().required(),
+  building_name: Joi.string().required(),
+  room_number: Joi.string().required(),
+  room_name: Joi.string().required(),
+});
 
 export class UpdateRoomDto extends PartialType(CreateRoomDto) {}
